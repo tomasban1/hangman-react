f583eb983bd66518c3691eac7c39a7eda8122412
tomasban1 <tomasbanius@gmail.com>
2024-08-11 16:32:39 +0300

    removed keypress if game over

--- src/components/checkLetter/CheckLetter.jsx
+++ src/components/checkLetter/CheckLetter.jsx
@@ -15,20 +15,44 @@ export function CheckLetter(){
     ? <ul key={index} className="wordLetter">{letter}</ul> 
     : <ul key={index} className="wordLetter"></ul>);
 
-        
+    let [count, setCount] = useState(6);
+    function loseLife(){
+        setCount(count - 1)
+    }
 
+  
+    const Keyboard =
+        keyboard.map((letter) => <button onClick={(e) => {if(randomWord.includes(letter) && gameOver === false){
+                    setCorrect([...correct, letter])
+                    e.currentTarget.classList.add('highlightCorrect')
+                }else{if(gameOver === false){e.currentTarget.classList.add('highlightIncorrect')
+                    loseLife()
+                }}}} className="boardBtn" key={letter}>{letter}</button>)
+    
+    let gameOver = false; 
+    const Counter = () => {
+        if(count < 1){
+            count = 0;
+            gameOver = true;
+            return <p>Game over!</p>
+        }else{
+            return <p>{count}</p>
+        }
+    }
+    
+    
     return (
         <>
             <div className="letterContainer">
                 {hiddenWord}
             </div>
             <div className="keyboardContainer">
-                {keyboard.map((letter) => <button onClick={(e) => {if(randomWord.includes(letter)){
-                    setCorrect([...correct, letter])
-                    e.currentTarget.classList.add('highlightCorrect')
-                }else{e.currentTarget.classList.add('highlightIncorrect')}}} className="boardBtn" key={letter}>{letter}</button>)}
+                {Keyboard}
+                <Counter />
             </div>
            
         </>    
     );
 }
+
+
