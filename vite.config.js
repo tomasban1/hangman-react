79509b2113696362b172f911a770e8e27d311eda
tomasban1 <tomasbanius@gmail.com>
2024-08-12 17:04:43 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/hangman-react/'
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/hangman-react/'
+  }
+
+  return config
 })
