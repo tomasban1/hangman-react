84b86502d0a45741edbfad3ad7a05c0206a85f7a
tomasban1 <tomasbanius@gmail.com>
2024-08-12 17:12:51 +0300

    still no succes :(

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/hangman-react/'
-  }
-
-  return config
+export default defineConfig({
+  plugins: [react()],
+  base: "/hangman-react",
 })
